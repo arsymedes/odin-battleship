77b30ed1a3f3b2b7a3879d99eb9e3c8fc2e9ed8a
arsyahmad12 <arsyahmad14@gmail.com>
2022-07-28 04:19:36 +0700

    Finished Gameboard

--- src/index.js
+++ src/index.js
@@ -1,29 +1,74 @@
-
-const Ship = (length) => {
-  const hitpoints = Array(length).fill(0)
+const Ship = (length, align, xCor, yCor) => {
+  const hitpoints = Array(length).fill(0);
 
   function hit(position) {
-    hitpoints[position] = 1
+    hitpoints[position] = 1;
   }
 
   function isSunk() {
-    if (hitpoints.includes(0)) return false
-    return true
+    if (hitpoints.includes(0)) return false;
+    return true;
   }
 
   return {
     length,
+    align,
+    xCor,
+    yCor,
     hit,
     isSunk,
-  }
-}
+  };
+};
 
 const Gameboard = (() => {
-  
+  const ships = [
+    Ship(5, "horizontal", 0, 0), // Ship(length, align, xCor, yCor)
+    Ship(3, "horizontal", 3, 2),
+    Ship(2, "vertical", 4, 5),
+  ];
+
+  const missedAttacks = [];
+
+  function checkAttack(xHit, yHit) {
+    let flag = false;
+    ships.forEach((ship) => {
+      if (
+        ship.yCor === yHit &&
+        ship.align === "horizontal" &&
+        ship.xCor <= xHit &&
+        xHit < ship.xCor + ship.length
+      ) {
+        flag = true;
+        ship.hit(xHit - ship.xCor);
+      }
+      if (
+        ship.xCor === xHit &&
+        ship.align === "vertical" &&
+        ship.yCor <= yHit &&
+        yHit < ship.yCor + ship.length
+      ) {
+        flag = true;
+        ship.hit(yHit - ship.yCor);
+      }
+    });
+    return flag;
+  }
+
+  function receiveAttack(xHit, yHit) {
+    if (checkAttack(xHit, yHit) === false) {
+      missedAttacks.push([xHit, yHit]);
+    }
+  }
+
+  function allShipSunk() {
+    return ships.every((ship) => ship.isSunk());
+  }
 
   return {
+    checkAttack,
+    receiveAttack,
+    allShipSunk,
+  };
+})();
 
-  }
-})()
-
-export { Ship, Gameboard }
+export { Ship, Gameboard };
--- src/index.test.js
+++ src/index.test.js
@@ -1,21 +1,59 @@
 import { Ship, Gameboard } from "./index"
 
 test('Ship: Length', () => {
-  const ship = Ship(5)
+  const ship = Ship(5, null, null, null)
   expect(ship.length).toBe(5)
 });
 
 test('Ship: Ship is not sunk', () => {
-  const ship = Ship(5)
+  const ship = Ship(5, null, null, null)
   ship.hit(2)
   ship.hit(3)
   expect(ship.isSunk()).toBe(false)
 });
 
 test('Ship: Ship is sunk', () => {
-  const ship = Ship(3)
+  const ship = Ship(3, null, null, null)
   ship.hit(0)
   ship.hit(2)
   ship.hit(1)
   expect(ship.isSunk()).toBe(true)
+});
+
+test('Gameboard: Attack Received', () => {
+  expect(Gameboard.checkAttack(0, 0)).toBe(true)
+});
+
+test('Gameboard: Attack Received, edge case', () => {
+  expect(Gameboard.checkAttack(4, 0)).toBe(true)
+});
+
+test('Gameboard: Attack Received, edge case 2', () => {
+  expect(Gameboard.checkAttack(5, 0)).toBe(false)
+});
+
+test('Gameboard: Attack Received, vertical', () => {
+  expect(Gameboard.checkAttack(4, 6)).toBe(true)
+});
+
+test('Gameboard: Attack Received, vertical edge case', () => {
+  expect(Gameboard.checkAttack(4, 7)).toBe(false)
+});
+
+test('Gameboard: All Ship Not Sunk', () => {
+  expect(Gameboard.allShipSunk()).toBe(false)
+});
+
+test('Gameboard: All Ship Sunk', () => {
+  Gameboard.receiveAttack(0, 0)
+  Gameboard.receiveAttack(1, 0)
+  Gameboard.receiveAttack(2, 0)
+  Gameboard.receiveAttack(3, 0)
+  Gameboard.receiveAttack(4, 0)
+  Gameboard.receiveAttack(3, 2)
+  Gameboard.receiveAttack(4, 2)
+  Gameboard.receiveAttack(5, 2)
+  Gameboard.receiveAttack(4, 5)
+  Gameboard.receiveAttack(4, 6)
+  expect(Gameboard.allShipSunk()).toBe(true)
 });
